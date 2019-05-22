41ca9ca07464988c40d7f561de20351af120781a
zhangjikuan <zhangjikuan@winchannel.net>
2019-05-22 16:52:15 +0800

    Initial commit

---
+++ Example/ZJKBaseSDK/ZJKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZJKAppDelegate.h
+//  ZJKBaseSDK
+//
+//  Created by k721684713@163.com on 05/22/2019.
+//  Copyright (c) 2019 k721684713@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZJKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZJKBaseSDK/ZJKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZJKViewController.h
+//  ZJKBaseSDK
+//
+//  Created by k721684713@163.com on 05/22/2019.
+//  Copyright (c) 2019 k721684713@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZJKViewController : UIViewController
+
+@end
